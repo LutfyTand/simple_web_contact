f8585ba63e956db5d7b8012150f6af9473286480
lutfi.tanjung <lutfi.tanjung@cashbac.com>
2022-05-22 22:04:36 +0700

    add load test for view data contacts

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,18 @@
-from locust import HttpUser, task
+import time
+from locust import HttpUser, task, between
 
-class HelloWorld(HttpUser):
+class startContact(HttpUser):
+    wait_time = between(1, 5)
     @task
-    def hello_world(self):
-        self.client.get("/")
+    def get_contact(self):
+        self.client.get("/health")
+        self.client.get("/contacts")
+    
+    @task(3)
+    def view_items(self):
+        for contact_id in range(10):
+            self.client.get(f"/contacts/{contact_id}", name="/contacts")
+            time.sleep(1)
+    
+    def on_start(self):
+        self.client.post("/contacts", json={"contact_id":0,"firstname":"Adas","lastname":"Bryan","email":"Adas.Bryan@contact.com","phone":"62856555378"})
